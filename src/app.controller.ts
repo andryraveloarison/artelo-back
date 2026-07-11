15e7361e324a55cdf3e7d1b983be7949fc01296f
Andry Raveloarison <andryraveloarison01@gmail.com>
2026-07-11 22:25:04 +0300

    fix timer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('time')
+  getServerTime(): { serverTime: number } {
+    return { serverTime: Date.now() };
+  }
 }
